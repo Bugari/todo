9696c39de7e095e3e02513d55bc987f410ee887e
Bugari <igornaw@gmail.com>
2021-07-26 19:52:20 +0200

    chore: refactored commands do and rm

--- cmd/do_test.go
+++ cmd/do_test.go
@@ -10,18 +10,14 @@ import (
 )
 
 var _ = Describe("HandleDone", func() {
-	prios := []string{"High", "Medium", "Low"}
-	tasksNames := []string{"a", "b", "c"}
-
+	var tasks = []db.Task{{Seq: 1, Name: "a"}, {Seq: 2, Name: "b"}, {Seq: 3, Name: "c"}}
 	BeforeEach(func() {
 		db.OpenDb(":memory:")
-		for idx, task := range tasksNames {
-			HandleAdd(task, prios[idx])
-		}
+		db.Conn.Create(&tasks)
 	})
 	Context("Marking task as done", func() {
 		It("should have tasks in db", func() {
-			var countExpected int64 = int64(len(tasksNames))
+			var countExpected int64 = int64(len(tasks))
 			var countGot int64 = -1
 
 			db.Conn.Model(&db.Task{}).Where("1 = 1").Count(&countGot)
@@ -29,36 +25,38 @@ var _ = Describe("HandleDone", func() {
 			Expect(countExpected).To(Equal(countGot), "DB tasks count do not match")
 		})
 
+		It("should not be done by default", func() {
+			var task db.Task
+			db.Conn.Model(&db.Task{}).First(&task)
+
+			Expect(task.Done).To(BeZero(), "Done field is nil")
+		})
 		It("should have all tasks not done yet", func() {
 			arr := []db.Task{}
 			result := db.Conn.Model(&db.Task{}).Find(&arr, "done is null")
-			Expect(result.RowsAffected).To(Equal(int64(len(tasksNames))), "DB should have all tasks not marked as done")
+			Expect(result.RowsAffected).To(Equal(int64(len(tasks))), "DB should have all tasks not marked as done")
 		})
 
 		It("should mark first task as done", func() {
-			arr := []db.Task{}
-			db.Conn.Model(&db.Task{}).Find(&arr, "done is null")
+			var task db.Task
+			db.Conn.Model(&db.Task{}).First(&task)
 
-			Expect(arr[0].Done).To(BeZero(), "Done field is nil")
+			changedTask, _ := HandleDone(1)
 
-			HandleDone(&arr[0])
-
-			Expect(arr[0].Done).NotTo(BeZero(), "Done field is not nil")
+			Expect(changedTask.Done).NotTo(BeZero(), "Done field is not nil")
 		})
 
-		for i, name := range tasksNames {
+		for i, task := range tasks {
 			It(fmt.Sprintf("should not return task #%d\n", i), func() {
 				arr := []db.Task{}
 				db.Conn.Model(&db.Task{}).Find(&arr, "done is null")
 
-				Expect(arr[i].Done).To(BeZero(), "Done field is nil")
-
-				HandleDone(&arr[i])
+				HandleDone(task.Seq)
 
 				db.Conn.Model(&db.Task{}).Find(&arr, "done is null")
-				Expect(len(arr)).To(Equal(len(tasksNames)-1), "Done field is not nil")
+				Expect(len(arr)).To(Equal(len(tasks)-1), "Done field is not nil")
 
-				Expect(arr).NotTo(WithTransform(pickOnlyNames, ContainElement(name)), fmt.Sprintf("task should not be returned anymore %d\n", i))
+				Expect(arr).NotTo(WithTransform(pickOnlyNames, ContainElement(task.Name)), fmt.Sprintf("task should not be returned anymore %d\n", i))
 			})
 
 		}
